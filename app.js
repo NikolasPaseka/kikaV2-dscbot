8124aa3f71bc74860b7e9288320b9deb0e85deb3
Nikolas Paseka <nikolas.paseka7@gmail.com>
2022-07-08 13:55:18 +0200

    redeploy

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ for (const file of slashFiles) {
 }
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}`)
+    console.log(`Ready. Logged in as ${client.user.tag}`)
 })
 
 client.on('messageCreate', (message) => {
