e863155587489c31c1958942d8cb5d6a2574cd01
Nikolas Paseka <nikolas.paseka7@gmail.com>
2022-09-19 15:46:01 +0200

    fixed stop command

--- slash/skip.js
+++ slash/skip.js
@@ -6,6 +6,10 @@ module.exports = {
     async execute(client, message) {
         const queue = client.distube.getQueue(message)
         if (!queue) return message.channel.send(`${client.emotes.error} | There is nothing in the queue right now!`)
+        if (queue.songs.length === 1) {
+            queue.stop()
+            return message.channel.send('Skipped! No more songs in the queue')
+        }
 
         try {
             const song = await queue.skip()
